001fafbd7a95c0e6aeba52304c24cc9319fe95e9
Guilherne Sousa <guilhermecamargodesousa@gmail.com>
2024-04-08 17:42:02 -0300

    teste

--- TRABAJOS-GURI.js
+++ TRABAJOS-GURI.js
@@ -2,7 +2,7 @@
 
 const peso1= 2, peso2 = 3, peso3 =  5; 
 
-const nota1 = 8, nota2 = 4, nota3 = 6;
+const nota1 = 10, nota2 =5, nota3 = 7;
 let resultado = calculaPonderado (nota1,nota2,nota3,peso1,peso2,peso3);
 function calculaPonderado (nota1,nota2,nota3,peso1,peso2,peso3) {
     const SomaDasNotas = ((nota1*peso1) + (nota2*peso2) + (nota3*peso3))/(peso1+peso2+peso3);
